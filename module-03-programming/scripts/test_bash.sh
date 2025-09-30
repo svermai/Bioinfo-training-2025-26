cef86272e462cec196075506c83625e085eb5967
Shubham <subhamverma844@gmail.com>
2025-09-30 14:41:03 +0400

    week2 & week3 updates

---
+++ module-03-programming/scripts/test_bash.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# echo is print 
+echo "Hello World"
+
+# create a folder
+mkdir folder_name
+
+# empty file inside a folder
+touch folder_name/file.txt
+
+# copy the file
+cp folder_name/file.txt folder_name/file1.txt
